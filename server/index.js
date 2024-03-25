84da9dc780957e215006990da40095052422f5ff
Tanwie12 <adeytanwie@gmail.com>
2024-03-25 10:27:17 -0700

    changed

--- server/index.js
+++ server/index.js
@@ -22,6 +22,6 @@ app.use(errorHandler);
 db();
 
 
-app.listen (3000, () => {
-    console.log('Server started on port 3000');
+app.listen (process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}! 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 � `);
 });

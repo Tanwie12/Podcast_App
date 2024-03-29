bda52ba410ebe245134bd50133b1b3de71d4f79c
Tanwie12 <adeytanwie@gmail.com>
2024-03-29 11:46:55 -0700

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {
   Navbar,
   Typography,
@@ -11,27 +11,16 @@ import { useSelector } from "react-redux";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@nextui-org/react";
 import { useDispatch } from "react-redux";
 import { useLogoutMutation } from "../slices/userApiSlice";
-import { logout } from "../slices/authSlice";
 
 export function NavBar1() {
   const { userInfo } = useSelector((state) => state.auth);
   const navigate=useNavigate()
   const dispatch=useDispatch()
-  const [logoutM]=useLogoutMutation()
-const handlePress= async()=>{
-  try {
-    dispatch(logout())
-   navigate('/login')
-    await logoutM().unwrap()
-  } catch (error) {
-    console.log(error)
-  }
-  
+  const [logout]=useLogoutMutation()
+const handlePress=()=>{
+  localStorage.removeItem("userInfo")
+  window.location.reload()
 }
-  
-  
-
-
   return (
     <div className="max-h-[768px] w-[calc(100%+48px)] bg-background">
       <Navbar className="sticky top-0 z-10 h-max max-w-full dark:bg-gray-500 rounded-none px-4 py-2 lg:px-8 lg:py-4">
@@ -90,4 +79,4 @@ const handlePress= async()=>{
       <Outlet />
     </div>
   );
-              }
+}

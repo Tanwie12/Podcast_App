db30bce95810f920b8d476a7c669d6196349d398
Tanwie12 <adeytanwie@gmail.com>
2024-03-29 12:12:28 -0700

    reverted changes of lohout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,21 +6,29 @@ import {
 import { Button as NButton } from "@nextui-org/react";
 import { DrawerDefault } from "./Drawerbar";
 import { UserCircleIcon } from "@heroicons/react/24/outline";
-import { Link, Outlet,} from "react-router-dom";
+import { Link, Outlet,useNavigate} from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Avatar } from "@nextui-org/react";
-// import { useDispatch } from "react-redux";
-// import { useLogoutMutation } from "../slices/userApiSlice";
+import { useDispatch } from "react-redux";
+import { useLogoutMutation } from "../slices/userApiSlice";
+import { logout } from "../slices/authSlice";
 
 export function NavBar1() {
   const { userInfo } = useSelector((state) => state.auth);
-  // const navigate=useNavigate()
-  // const dispatch=useDispatch()
-  // const [logout]=useLogoutMutation()
-const handlePress=()=>{
-  localStorage.removeItem("userInfo")
-  window.location.reload()
+  const navigate=useNavigate()
+  const dispatch=useDispatch()
+  const [logoutM]=useLogoutMutation()
+const handlePress=async()=>{
+  try{
+    await logoutM().unwrap()
+    dispatch(logout())
+    navigate('/')
+  }catch(err){
+    console.log(err)
+  }
 }
+
+
   return (
     <div className="max-h-[768px] w-[calc(100%+48px)] bg-background">
       <Navbar className="sticky top-0 z-10 h-max max-w-full dark:bg-gray-500 rounded-none px-4 py-2 lg:px-8 lg:py-4">

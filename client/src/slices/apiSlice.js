19c296c179e7bd6ae6013abfcd0e6d0a7baa0140
Tanwie12 <adeytanwie@gmail.com>
2024-03-29 09:29:27 -0700

    addjusted the route to web service

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,5 +1,5 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-const baseQuery=fetchBaseQuery({baseUrl:'http://localhost:5000'});
+const baseQuery=fetchBaseQuery({baseUrl:'https://podcast-app-uxb4.onrender.com'});
 
 export const apiSlice=createApi({
     baseQuery,

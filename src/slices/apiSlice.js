591fb2d8e8a8429a7ad8408375051e7fd799db49
Tanwie12 <adeytanwie@gmail.com>
2024-05-11 23:34:46 +0100

    ahnge env

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,11 +2,13 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 // const localUrl="http://127.0.0.1:5000/"
 // const cyclic="https://impossible-waders-calf.cyclic.app/"
 // const onrender='https://podcast-app-uxb4.onrender.com'
-const URL=process.env.NODE_ENV === "development"?process.env.LOCAL_URL:process.env.CYCLIC_URL;
+const URL=process.env.REACT_APP_NODE_ENV === "development"?process.env.REACT_APP_lOCAL_URL:process.env.REACT_APP_CYCLIC_URL;
+console.log('URL',process.env.REACT_APP_lOCAL_URL,process.env.REACT_APP_CYCLIC_URL,URL)
 const baseQuery = fetchBaseQuery({
     baseUrl: URL,
     credentials: 'include', // Include cookies with the request
   });
+  console.log(process.env);
 
 export const apiSlice=createApi({
     baseQuery,
